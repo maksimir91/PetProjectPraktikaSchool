a196b349e401e4c6189f496f1116fbbf70154e10
maksimir91 <maksimirrus@gmail.com>
2024-01-02 15:56:43 +0300

    Initial Commit

---
+++ PetProjectPraktikaSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetProjectPraktikaSchool
+//
+//  Created by Stanislav Shut on 02.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
